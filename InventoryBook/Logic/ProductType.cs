aba4bcc2231cca5f75911bf76d1381c5e37d3601
elvolk <elvolk@ttu.ee>
2017-02-23 10:04:51 +0200

    ...

--- InventoryBook/Logic/ProductType.cs
+++ InventoryBook/Logic/ProductType.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InventoryBook.Logic
 {
-    class ProductType
+    public class ProductType
     {
+        public string ProductName { get; set; }
+        public string Colour { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public int Weight { get; set; }
+        public string Category { get; set; }
+        public DateTime Year { get; set; }
     }
 }
