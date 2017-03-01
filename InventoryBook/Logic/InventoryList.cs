8ae852b6219a6f95152552ef630b5ee192c6e9c7
elvolk <elvolk@ttu.ee>
2017-03-01 17:17:07 +0200

    .

--- InventoryBook/Logic/InventoryList.cs
+++ InventoryBook/Logic/InventoryList.cs
@@ -10,11 +10,11 @@ namespace InventoryBook.Logic
             double InventoryStatus, DateTime ProductValidUntil)
         {
             List<Product> ProductList = new List<Product>();
-            
+            var result = ProductList.Find(Product => ItemName == "ItemName");
 
             if (ProductList != null)
             {
-                ProductList.Add(new Product("ItemName", 10, 1265, "TOOTED1", 667, "Hea toode", 1,new DateTime(2017, 03, 03)));
+                ProductList.Add(new Product("ItemName", 10, 1265, "TOOTED1", 667, "Hea toode", 1, new DateTime(2017, 03, 03)));
             }
             
 
