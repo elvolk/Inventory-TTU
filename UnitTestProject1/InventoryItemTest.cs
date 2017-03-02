7bcb798984d10555cf7d2ab45ab27b8a34bf50ee
ElinaTubli <kzaitsevest@gmail.com>
2017-03-02 09:27:16 +0200

    ,

---
+++ UnitTestProject1/InventoryItemTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InventoryBook.Logic;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace InventoryBook.Tests
+{
+    [TestClass]
+    public class InventoryItemTest
+    {
+        private InventoryItem i;
+        [TestInitialize]
+        public void Init()
+        {
+            i = new InventoryItem();
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(i);
+        }
+        
+
+    }
+}
