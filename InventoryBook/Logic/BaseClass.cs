39c27080a43d7b1795f28fc82d78446899655372
Siim <Siim@DESKTOP-4DP41U7>
2017-03-02 11:02:29 +0200

    .

---
+++ InventoryBook/Logic/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryBook.Logic
+{
+    public class BaseClass
+    {
+        public void SetValue<T>(ref T field, T value)
+        {
+            field = value;
+        }
+    }
+}
